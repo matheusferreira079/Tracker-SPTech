ad248f572072f4a66c27d58c6baeba251e1faad3
matheusferreira079 <matheus.matos@bandtec.com.br>
2022-06-24 14:22:50 -0300

    upando projeto

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
+    <div class="column is-one-quarter">
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
+    </div>
+    <div class="column is-three-quarter conteudo">
+      <Formulario @aoSalvarTarefa="salvarTarefa" />
+      <div class="lista">
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Box v-if="listaEstaVazia">
+          <span class="text-negado">Você não está muito produtivo hoje :(</span>
+        </Box>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
+import Tarefa from './components/Tarefa.vue';
+import ITarefa from './interfaces/ITarefa'
+import Box from './components/Box.vue';
+
+export default defineComponent({
+  name: "App",
+  components: {
+    BarraLateral,
+    Formulario,
+    Tarefa,
+    Box
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    },
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo
+    }
+  }
+});
+</script>
+
+<style>
+.lista {
+  padding: 1.25rem;
+}
+
+main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+
+main.modo-escuro {
+  --bg-primario: #1F2A44;
+  --texto-primario: #fff;
+}
+
+.conteudo{
+  background-color: var(--bg-primario);
+}
+
+.text-negado{
+  color: #ED145B;
+}
+</style>
